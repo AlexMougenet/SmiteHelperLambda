15a52b69f9c690d038cfb858a369da6f31aa4f03
Alex Mougenet <alex@MacBook-Pro-de-Alex.local>
2019-11-24 03:25:54 +0100

    feat: big clean

--- handlers/buildIntent.js
+++ handlers/buildIntent.js
@@ -2,11 +2,10 @@ const i18n = require('../i18n');
 const req = require('../request');
 const util = require('../util');
 
-const handle = handlerInput => {
+const handle = async handlerInput => {
   const isYesIntent = handlerInput.requestEnvelope.request.intent.name === "AMAZON.YesIntent";
   const attributes = handlerInput.attributesManager.getSessionAttributes();
-  const lang = handlerInput.requestEnvelope.request.locale.split('-')[0].toLowerCase();
-  i18n.setLocale(lang);
+  const lang = attributes.lang || handlerInput.requestEnvelope.request.locale.split('-')[0].toLowerCase();
   let god;
   let queue;
   if (isYesIntent) {
@@ -24,64 +23,51 @@ const handle = handlerInput => {
   const queueId = util.getQueueIdByQueue(queue, lang);
   
   let speakOutput;
+  let repromptSpeech;
 
   if (queueId && normalizedGod) {
     if (!isYesIntent) {
-      return req.httpGet(`https://api.smite.guru/v3/champions/${normalizedGod}/builds`).then(res => {
-        const itemId = (i) => {
-          if (!!res['builds'][queueId][`slot-${i}`]['primary']) {
-            return util.getItemById(res['builds'][queueId][`slot-${i}`]['primary']['item'], lang);
-          } else {
-            return {DeviceName: 'null'};
-          }
+      let res = await req.httpGet(`https://api.smite.guru/v3/champions/${normalizedGod}/builds`)
+      const itemId = (i) => {
+        if (!!res['builds'][queueId][`slot-${i}`]['primary']) {
+          return util.getItemById(res['builds'][queueId][`slot-${i}`]['primary']['item'], lang);
+        } else {
+          return {DeviceName: 'null'};
         }
-        if (!isYesIntent) {
-          const item1 = itemId(1);
-          const item2 = itemId(2);
-          const item3 = itemId(3);
-          attributes.item4 = itemId(4);
-          attributes.item5 = itemId(5);
-          attributes.item6 = itemId(6);
-          handlerInput.attributesManager.setSessionAttributes(attributes);
-          speakOutput = i18n.tmf('BUILD.RESULT.3_FIRST_ITEMS', {god: god, queue: queue, item1: item1.DeviceName, item2: item2.DeviceName, item3: item3.DeviceName, });
-        }
-  
-        const repromptSpeech = i18n.t('BUILD.RESULT.CONTINUE');
-  
-        return handlerInput.responseBuilder
-          .speak(speakOutput)
-          .withShouldEndSession(true)
-          .reprompt(repromptSpeech)
-          .getResponse();
-      }).catch(e => {
-          speakOutput = i18n.tmf('ERROR.UNKNOWN', {error: JSON.stringify(e)});
-          console.log('ERROR', JSON.stringify(e));
-          return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .withShouldEndSession(false)
-            .getResponse();
-      });
+      }
+      const item1 = itemId(1);
+      const item2 = itemId(2);
+      const item3 = itemId(3);
+      attributes.item4 = itemId(4);
+      attributes.item5 = itemId(5);
+      attributes.item6 = itemId(6);
+      handlerInput.attributesManager.setSessionAttributes(attributes);
+
+      speakOutput = i18n.tmf('BUILD.RESULT.3_FIRST_ITEMS', {god: god, queue: queue, item1: item1.DeviceName, item2: item2.DeviceName, item3: item3.DeviceName, });
+      repromptSpeech = i18n.t('BUILD.RESULT.CONTINUE');
+      
     } else {
-      const item4 = attributes.item4;
-      const item5 = attributes.item5;
-      const item6 = attributes.item6 ;
+      const item4 = attributes.item4 || {DeviceName: null};
+      const item5 = attributes.item5 || {DeviceName: null};
+      const item6 = attributes.item6 || {DeviceName: null};
       speakOutput = i18n.tmf('BUILD.RESULT.3_LAST_ITEMS', {item4: item4.DeviceName, item5: item5.DeviceName, item6: item6.DeviceName, });
-      return handlerInput.responseBuilder
-        .speak(speakOutput)
-        .withShouldEndSession(false)
-        .getResponse();
+      repromptSpeech = i18n.t('DO_YOU_NEED_MORE');
     }
     
-  } else if (!normalizedGod && ! queueId) {
-    speakOutput = i18n.tmf('ERROR.NO_GOD_NO_QUEUE', {god: god, queue: queue});
-  } else if (!normalizedGod) {
-    speakOutput = i18n.tmf('ERROR.NO_GOD', {god: god});
-  } else if (!queueId) {
-    speakOutput = i18n.tmf('ERROR.NO_QUEUE', {queue: queue});
-  }
+  } else {
+    if (!normalizedGod && ! queueId) {
+      speakOutput = i18n.tmf('ERROR.NO_GOD_NO_QUEUE', {god: god, queue: queue});
+    } else if (!normalizedGod) {
+      speakOutput = i18n.tmf('ERROR.NO_GOD', {god: god});
+    } else if (!queueId) {
+      speakOutput = i18n.tmf('ERROR.NO_QUEUE', {queue: queue});
+    }
+    repromptSpeech = i18n.t('HELP');
+  } 
   return handlerInput.responseBuilder
     .speak(speakOutput)
-    .withShouldEndSession(false)
+    .withShouldEndSession(true)
+    .reprompt(repromptSpeech)
     .getResponse();
 }
 
